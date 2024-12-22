75ba55040adaf6c334f242faab11be121b4dc10a
ajayjnv <ajaykumarkontala@gmail.com>
2024-12-22 09:41:35 +0530

    My projects

---
+++ project.js
@@ -0,0 +1,8 @@
+let logo=document.getElementById("logo")
+let image=document.getElementById("myphoto")
+let rr=document.getElementById("rr")
+
+logo.addEventListener("click",()=>{
+    image.style.display="flex"
+    rr.style.display="none"
+})
